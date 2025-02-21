7ef1744f9cc03025e9d575f1f8faed997f74d859
krushil shah <krushilshah06@gmail.com>
2025-02-21 21:33:52 +0530

    page migration and modal code

---
+++ app/Models/Page.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    
+    public function parent()
+    {
+        return $this->belongsTo(Page::class, 'parent_id');
+    }
+
+    
+    public function children()
+    {
+        return $this->hasMany(Page::class, 'parent_id');
+    }
+
+    public function descendants()
+    {
+        return $this->children()->with('descendants');
+    }
+}
